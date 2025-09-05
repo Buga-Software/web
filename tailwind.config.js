ca7c2da217a00bb20f87b92dfbd50caabeeef1c3
Gothsec <oscarandreshernandezpineda@gmail.com>
2025-09-05 09:13:34 -0500

    New(Design branch)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Satoshi: ['Satoshi', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
